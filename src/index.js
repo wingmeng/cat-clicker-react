15de2ac521d10e1d94dff04a26f659d2012dfc90
wingmeng <wingmeng@qq.com>
2019-03-26 15:42:37 +0800

    :tada:

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';  // 解析、编译器
-import ReactDOM from 'react-dom';  // 渲染到 DOM 的库
-import './index.css';  // 样式文件
-import App from './App';  // 根组件
+import React from 'react'  // 解析、编译器
+import ReactDOM from 'react-dom'  // 渲染到 DOM 的库
+import './index.css'  // 样式文件
+import App from './App'  // 根组件
 
 // 渲染到 DOM 中
 ReactDOM.render(<App />, document.getElementById('root'));
