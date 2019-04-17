ecedc1e2d8dc2f649f69e4b5ad6dcf97957e2390
wingmeng <wingmeng@qq.com>
2019-04-17 09:37:07 +0800

    :bookmark:

---
+++ build/precache-manifest.0d73f170a6bd9c9568a08e261c719d62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "3def22936d24b58fa702",
+    "url": "./static/js/main.b5eb1ee0.chunk.js"
+  },
+  {
+    "revision": "ac164eeb7fe39897100f",
+    "url": "./static/js/2.aa7a0768.chunk.js"
+  },
+  {
+    "revision": "3def22936d24b58fa702",
+    "url": "./static/css/main.f82a3b32.chunk.css"
+  },
+  {
+    "revision": "47dbb444e8e19905220055c6b1319f6d",
+    "url": "./index.html"
+  }
+];
